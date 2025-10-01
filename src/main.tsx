0d3cce892a631fa180a19d27f47aab849da0401e
Pritam Sarkar <162453679+PritamSarkar12212004@users.noreply.github.com>
2025-10-02 00:35:48 +0530

    make more changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import '../src/styles/index.css'
-import { BrowserRouter } from 'react-router-dom'
-import MainRoutes from './routes/MainRoutes.tsx'
+import { createRoot } from 'react-dom/client';
+import '../src/styles/index.css';
+import { BrowserRouter } from 'react-router-dom';
+import MainRoutes from './routes/MainRoutes';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <MainRoutes />
   </BrowserRouter>
-)
+);

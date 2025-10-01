f2bc6d6a15aec619c4cbd10613e3626b0d0c4c16
Pritam Sarkar <162453679+PritamSarkar12212004@users.noreply.github.com>
2025-10-02 00:19:48 +0530

    add soemn more feaure account delete

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import PrivacyNPolicy from '../page/artical/PrivacyNPolicy'
+import AccountDeletePage from '../page/request/AccountDeletePage'
 
 function MainRoutes() {
   return (
     <Routes>
       <Route Component={PrivacyNPolicy} path='/policy' />
+      <Route Component={AccountDeletePage} path='/delete/account' />
     </Routes>
   )
 }

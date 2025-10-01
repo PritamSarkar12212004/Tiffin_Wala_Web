c92a11596f83f6f742a79af855b4c6831452bc2f
Pritam Sarkar <162453679+PritamSarkar12212004@users.noreply.github.com>
2025-10-02 00:23:46 +0530

    all set

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,14 +1,14 @@
-import { Route, Routes } from 'react-router-dom'
-import PrivacyNPolicy from '../page/artical/PrivacyNPolicy'
-import AccountDeletePage from '../page/request/AccountDeletePage'
+import { Route, Routes } from 'react-router-dom';
+import PrivacyNPolicy from '../page/artical/PrivacyNPolicy';
+import AccountDeletePage from '../page/request/AccountDeletePage';
 
 function MainRoutes() {
   return (
     <Routes>
-      <Route Component={PrivacyNPolicy} path='/policy' />
-      <Route Component={AccountDeletePage} path='/delete/account' />
+      <Route path='/policy' element={<PrivacyNPolicy />} />
+      <Route path='/delete/account' element={<AccountDeletePage />} />
     </Routes>
-  )
+  );
 }
 
-export default MainRoutes
+export default MainRoutes;
